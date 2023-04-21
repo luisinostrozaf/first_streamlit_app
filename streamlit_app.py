7b88f6939a15c5e68a522350cc7d9f81ec86c98a
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2023-04-21 17:08:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit 
+streamlit.title("MY PARENTS NEW HEALTHY DIET")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
